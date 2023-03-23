85eae8de0a28df744a6635ba943d157d115f11cb
Ariane Kevinny <arianekevinny@gmail.com>
2023-03-23 12:57:12 -0300

    Subindo APP

---
+++ RACHIM/App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>Olá</Text>
+    </View>
+  );
+}
+
+export default App;
