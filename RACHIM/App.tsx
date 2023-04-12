5f52f480b3262008ec4abf118c905b40da921fff
Ariane Kevinny <arianekevinny@gmail.com>
2023-04-12 09:29:51 -0300

    Arquivo APP modificado - Correção de Bugs

--- RACHIM/App.tsx
+++ RACHIM/App.tsx
@@ -1,27 +1,26 @@
-import * as React from 'react'; 
-import { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
-import auth from '@react-native-firebase/auth'
-import { View, Text } from '@react-native'
-
+import auth from '@react-native-firebase/auth';
 //Telas
 import Home from './src/Screens/home';
 import LoginScreen from './src/Screens/loginScreen';
+import {View, Text} from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
-  
-  //Configurando a autenticação utilizando o módulo auth do FireBase 
+  //Configurando a autenticação utilizando o módulo auth do FireBase
   //'Setando' os estados iniciais com conexões Firebase
-  
+
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState();
 
-  function onAuthStateChanged(user){
+  function onAuthStateChanged(user) {
     setUser(user);
-    if (initializing) setInitializing(false)
+    if (initializing) {
+      setInitializing(false);
+    }
   }
 
   useEffect(() => {
@@ -29,7 +28,9 @@ function App(): JSX.Element {
     return subscriber; // unsubscribe on unmount
   }, []);
 
-    if (initializing) return null;
+  if (initializing) {
+    return null;
+  }
 
   if (!user) {
     return (
@@ -39,25 +40,21 @@ function App(): JSX.Element {
     );
   }
 
-
   return (
     <View>
       <Text>Bem Vindo, {user.email}</Text>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            options={{title: 'Principal'}}
+            component={Home}
+          />
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          options={{ title: 'Principal' }}
-          component={Home} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
   );
 }
 
 export default App;
-function useEffect(arg0: () => any, arg1: undefined[]) {
-  throw new Error('Function not implemented.');
-}
-
