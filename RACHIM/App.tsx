6590182cf4fb0e6fd8eb7f3dd1464a404bfaacdf
Ariane Kevinny <arianekevinny@gmail.com>
2023-04-11 21:30:47 -0300

    Subindo conexão com firebase

--- RACHIM/App.tsx
+++ RACHIM/App.tsx
@@ -1,18 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import {Text, View} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+import Home from './src/Screens/home';
+import LoginScreen from './src/Screens/loginScreen';
+
+const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
-    <View>
-      <Text>Olá</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          options={{title: 'Principal'}}
+          component={Home}
+        />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
