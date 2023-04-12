a0ba3214d895f2b3550ad62e1f1612ea12d77650
Ariane Kevinny <arianekevinny@gmail.com>
2023-04-11 22:12:37 -0300

    Modificando parte da autenticação do usuário

--- RACHIM/App.tsx
+++ RACHIM/App.tsx
@@ -1,14 +1,47 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
-
+import auth from '@react-native-firebase/auth'
+import { View, Text } from '@react-native'
 import Home from './src/Screens/home';
 import LoginScreen from './src/Screens/loginScreen';
 
 const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
+  //Configurando a autenticação utilizando o módulo auth do FireBase 
+
+  //'Setando' os estados iniciais com conexões Firebase
+  
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  function onAuthStateChanged(user){
+    setUser(user);
+    if (initializing) setInitializing(false)
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+    if (initializing) return null;
+
+  if (!user) {
+    return (
+      <View>
+        <Text>Login</Text>
+      </View>
+    );
+  }
+
+
   return (
+    <View>
+      <Text>Bem Vindo, {user.email}</Text>
+    </View>
+
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
@@ -23,3 +56,7 @@ function App(): JSX.Element {
 }
 
 export default App;
+function useEffect(arg0: () => any, arg1: undefined[]) {
+  throw new Error('Function not implemented.');
+}
+
